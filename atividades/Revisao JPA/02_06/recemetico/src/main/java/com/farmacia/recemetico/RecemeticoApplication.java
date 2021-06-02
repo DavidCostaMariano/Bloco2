98556df5bd88715186f991de932b915ec03cec6d
DavidCostaBKE <daviddacostamariano@outlook.com.br>
2021-06-02 10:31:18 -0300

    02_06

---
+++ atividades/Revisao JPA/02_06/recemetico/src/main/java/com/farmacia/recemetico/RecemeticoApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.recemetico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecemeticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecemeticoApplication.class, args);
+	}
+
+}
---
+++ atividades/Revisao JPA/02_06/recemetico/src/test/java/com/farmacia/recemetico/RecemeticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.recemetico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecemeticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
